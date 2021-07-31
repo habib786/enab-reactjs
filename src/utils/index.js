b3cad399bd18fa47c53d223774a9bed3e00bf9fd
Habib Muhammad Farooq <habib-m-farooq@hotmail.com>
2021-07-31 23:35:48 +0500

    introduced util method to generate repo list

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,17 @@
+import map from "lodash/map";
+
 export const getDateOfLastMonth = () => {
   var date = new Date();
   date.setDate(date.getDate() - 30);
   return date.toISOString().split("T")[0];
 };
+
+export const getLanguageRepos = (reposList) =>
+  map(reposList, (repo) => ({
+    repositorie_name: repo["name"],
+    owner_name: repo["owner"]["login"],
+    repositorie_url: repo["html_url"],
+    forks: repo["forks"],
+    watchers: repo["watchers"],
+    stars: repo["stargazers_count"],
+  }));
